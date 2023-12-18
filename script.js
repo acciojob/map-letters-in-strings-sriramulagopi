d8e0a3da57c86fb3d647e9bfe55aad3e56702c0e
sriramulagopi <129825285+sriramulagopi@users.noreply.github.com>
2023-12-18 10:25:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function mapletters(str) {
+	let obj={};
+	for (let i=0;i<str.length;i++){
+		obj[str[i]]=[];
+		for (let j=0;j<str.length;j++){
+			if (str[i]===str[j]){
+				obj[str[i]].push(j);
+			}
+		}
+	}
+	console.log(obj);
+}
+mapletters("dodo");
